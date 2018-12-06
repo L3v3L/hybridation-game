bfaf25f6b257116ade0b55ac177ffa8ae5bf19a8
Ivo Barros <myL3v3L@gmail.com>
2018-12-06 00:07:10 +0000

    change port to not collide with other apps

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ module.exports = {
         }),
         new BrowserSyncPlugin({
             host: process.env.IP || 'localhost',
-            port: process.env.PORT || 3000,
+            port: process.env.PORT || 30303,
             server: {
                 baseDir: ['./', './build']
             }
