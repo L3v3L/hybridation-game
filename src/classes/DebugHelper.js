281426002f692030f8eb309200804d969cc872a0
Ivo Barros <myL3v3L@gmail.com>
2018-12-13 23:56:31 +0000

    created debug function to get hexagon by id

--- src/classes/DebugHelper.js
+++ src/classes/DebugHelper.js
@@ -6,4 +6,12 @@ export default class {
     changeCurrentPlayer (playerId) {
         this.game.global.CURRENT_PLAYER = playerId;
     }
+
+    getHexagon (id) {
+        for (let index = 0; index < this.game.global.ALL_CELLS.length; index++) {
+            if (this.game.global.ALL_CELLS[index].id === id) {
+                return this.game.global.ALL_CELLS[index].asset;
+            }
+        }
+    }
 }
